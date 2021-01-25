c5474ef27667fd74a6e676d7489cd6802c428904
west1636 <nishikawa@nii.ac.jp>
2021-01-25 18:59:09 +0900

    input Date (2-2)

--- app/guid-node/integromat/controller.ts
+++ app/guid-node/integromat/controller.ts
@@ -5,7 +5,7 @@ import { reads } from '@ember-decorators/object/computed';
 import { service } from '@ember-decorators/service';
 
 import DS from 'ember-data';
-
+import moment from 'moment';
 import IntegromatConfigModel from 'ember-osf-web/models/integromat-config';
 import Node from 'ember-osf-web/models/node';
 import Analytics from 'ember-osf-web/services/analytics';
@@ -49,7 +49,7 @@ export default class GuidNodeIntegromat extends Controller {
     selectedTargetId : string[] = [];
     microsoftTeamsMeetings : microsoftTeamsMeetings[] = [];
 
-    times = ['0:00', '1:00', '1:30', '2:00','2:30', '3:00', '3:30', '4:00', '4:30', '5:00', '5:30',, '6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00','13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30',, '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30'];
+    times = ['0:00', '1:00', '1:30', '2:00','2:30', '3:00', '3:30', '4:00', '4:30', '5:00', '5:30', '6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00','13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30',, '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30'];
 
 
     teams_subject = '';
@@ -269,13 +269,13 @@ export default class GuidNodeIntegromat extends Controller {
 
     @action
     setStartTime() {
-        startDate = document.querySelector('select[name=select-start-time]');
+        const startDate = document.querySelector('select[name=select-start-time]');
         this.set('teams_startTime', startDate);
     }
 
     @action
     setEndTime() {
-        endDate = document.querySelector('select[name=select-end-time]');
+        const endDate = document.querySelector('select[name=select-end-time]');
         this.set('teams_endTime', endDate);
     }
 

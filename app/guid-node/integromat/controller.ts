959e4906f1ddfc3273f6eed448421af0f2848c64
west1636 <shunta.nishikawa@ivis.co.jp>
2021-03-08 12:08:22 +0900

    for meeting delete(1)

--- app/guid-node/integromat/controller.ts
+++ app/guid-node/integromat/controller.ts
@@ -84,7 +84,7 @@ export default class GuidNodeIntegromat extends Controller {
     defaultStartTime = moment(this.currentTime.setMinutes(Math.round(this.currentTime.getMinutes() / 30) * 30)).format('HH:mm');
     defaultEndTime = moment(this.currentTime.setMinutes((Math.round(this.currentTime.getMinutes() / 30) * 30) + 60)).format('HH:mm');
 
-    times = ['0:00', '1:00', '1:30', '2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00', '5:30', '6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30'];
+    times = ['0:00', '0:30', '1:00', '1:30', '2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00', '5:30', '6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30'];
 
 
     teams_subject = '';
@@ -95,7 +95,8 @@ export default class GuidNodeIntegromat extends Controller {
     teams_endTime = '';
     teams_location = '';
     teams_content = '';
-    teams_meetingId = '';
+    teams_updateMeetingId = '';
+    teams_deleteMeetingId = '';
     teams_joinUrl = '';
 
     teamsMeetingAttendees : string[] = [];
@@ -280,7 +281,7 @@ export default class GuidNodeIntegromat extends Controller {
                 this.set('teams_endTime', moment(microsoftTeamsMeetings[i].fields.end_datetime).format('HH:mm'));
                 this.set('teams_location', microsoftTeamsMeetings[i].fields.location);
                 this.set('teams_content', microsoftTeamsMeetings[i].fields.content);
-                this.set('teams_meetingId', microsoftTeamsMeetings[i].fields.meetingid);
+                this.set('teams_updateMeetingId', microsoftTeamsMeetings[i].fields.meetingid);
                 this.set('teams_joinUrl', microsoftTeamsMeetings[i].fields.join_url);
                 break;
             }
@@ -329,7 +330,7 @@ export default class GuidNodeIntegromat extends Controller {
         const teams_end_date_time = teams_endDate + ' ' + teams_endTime;
         const teams_location = this.teams_location;
         const teams_content = this.teams_content;
-        const microsoft_teams_meeting_id = this.teams_meetingId;
+        const microsoft_teams_meeting_id = this.teams_updateMeetingId;
         const microsoft_teams_meeting_join_url = this.teams_joinUrl;
         const microsoftTeamsAttendeesChecked = document.querySelectorAll('input[class=microsoftTeamsAttendeesCheck]:checked');
 
@@ -343,6 +344,8 @@ export default class GuidNodeIntegromat extends Controller {
 
         const action = 'updateMicrosoftTeamsMeeting';
         const timestamp = new Date().getTime();
+
+        this.set('teams_updateMeetingId', '');
 
         const payload = {
             'nodeId': node_id,
@@ -403,23 +406,14 @@ export default class GuidNodeIntegromat extends Controller {
     }
 
     @action
-    makeDeleteDialog(this: GuidNodeIntegromat) {
-        const microsoftTeamsMeetingChecked = document.querySelectorAll('input[class=microsoftTeamsMeetingCheck]:checked');
+    makeDeleteDialog(this: GuidNodeIntegromat, id: string) {
 
-        if(microsoftTeamsMeetingChecked.length < 1){
-            this.toast.error(this.i18n.t('integromat.select_one_or_more'));
-        }else{
-            this.willDeleteMeetings.length = 0;
+        this.set('teams_deleteMeetingId', id);
 
-            for(let i = 0; i < microsoftTeamsMeetingChecked.length; i++){
-                this.willDeleteMeetings.push(((<HTMLElement>microsoftTeamsMeetingChecked[i]).dataset.subject || '').toString());
-            }
-            this.set('showDeleteMicrosoftTeamsMeetingDialog', true);
-        }
     }
 
     @action
-    startDeleteMicrosoftTeamsMeetingScenario(this: GuidNodeIntegromat, id: string) {
+    startDeleteMicrosoftTeamsMeetingScenario(this: GuidNodeIntegromat) {
 
         if (!this.config) {
             throw new EmberError('Illegal config');
@@ -434,7 +428,7 @@ export default class GuidNodeIntegromat extends Controller {
         const payload = {
             'nodeId': nodeId,
             'meetingAppName': appName,
-            'microsoftTeamsMeetingId': id,
+            'microsoftTeamsMeetingId': this.teams_deleteMeetingId,
             'action': action,
             'infoGrdmScenarioStarted': infoGrdmScenarioStarted,
             'infoGrdmScenarioCompleted': infoGrdmScenarioCompleted,
@@ -452,6 +446,7 @@ export default class GuidNodeIntegromat extends Controller {
             'timestamp': timestamp,
         };
 
+        this.set('teams_deleteMeetingId', '');
         this.set('showDeleteMicrosoftTeamsMeetingDialog', false);
 
         return fetch(

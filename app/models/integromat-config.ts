7c9fc9a675fdb712f7a7fd91dc1a6c2f5c9e1474
west1636 <nishikawa@nii.ac.jp>
2021-06-16 11:48:55 +0900

    revise integromat-config.ts

--- app/models/integromat-config.ts
+++ app/models/integromat-config.ts
@@ -2,7 +2,6 @@ import { attr } from '@ember-decorators/data';
 import OsfModel from './osf-model';
 
 export default class IntegromatConfigModel extends OsfModel {
-    @attr('string') webhook_url!: string;
     @attr('string') all_web_meetings!: string;
     @attr('string') upcoming_web_meetings!: string;
     @attr('string') previous_web_meetings!: string;

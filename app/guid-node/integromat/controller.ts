96c9e326065f3dd7f3d354fb3fcbad51f1290fb4
west1636 <nishikawa@nii.ac.jp>
2021-06-18 14:59:55 +0900

    revise controller.ts

--- app/guid-node/integromat/controller.ts
+++ app/guid-node/integromat/controller.ts
@@ -915,7 +915,7 @@ export default class GuidNodeIntegromat extends Controller {
         })
         .then(res => res.json())
         .then(data => {
-            else if(data.integromatMsg === 'integromat.info.completed'){
+            if(data.integromatMsg === 'integromat.info.completed'){
                 this.toast.info(this.i18n.t(data.integromatMsg));
                 this.save();
             }else if(data.integromatMsg.match('.error.')){

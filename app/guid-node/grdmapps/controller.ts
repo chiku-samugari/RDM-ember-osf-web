c79e2f13400b795bd67698b488be2c7f0f0b41d3
west1636 <nishikawa@nii.ac.jp>
2021-09-18 00:32:20 +0900

    integromat addon set(2-5)

--- app/guid-node/grdmapps/controller.ts
+++ app/guid-node/grdmapps/controller.ts
@@ -26,6 +26,12 @@ export default class GuidNodeGrdmapps extends Controller {
 
     configCache?: DS.PromiseObject<GrdmappsConfigModel>;
 
+    showWorkflows = true;
+    showWebMeetingWorkflow = false;
+
+
+    webhookUrl = '';
+
     @computed('config.isFulfilled')
     get loading(): boolean {
         return !this.config || !this.config.get('isFulfilled');
@@ -51,6 +57,215 @@ export default class GuidNodeGrdmapps extends Controller {
         config.rollbackAttributes();
         const message = this.intl.t('integromat.failed_to_save');
         this.toast.error(message);
+    }
+
+    camel2space(v: string) {
+
+        const separator = ' ';
+        return v
+                .replace(/[A-Z][a-z]/g, function (match) {
+                    return separator + match;
+                })
+                .replace(/[A-Z]+$/g, function (match) {
+                    return separator + match;
+                })
+                .trim();
+    }
+
+    @action
+    startMeeting(this: GuidNodeGrdmapps, v: string) {
+        window.open(v, '_blank');
+    }
+
+    @action
+    displayWorkflows(this: GuidNodeGrdmapps) {
+        this.set('showWorkflows', true);
+        this.set('showWebMeetingWorkflow', false);
+        this.set('webhookUrl', '');
+    }
+
+    @action
+    setWorkflow(this: GuidNodeGrdmapps, workflow_desp: string) {
+
+        const workflowType = workflow_desp.split('.')[2];
+
+        if (!this.config) {
+            throw new EmberError('Illegal config');
+        }
+
+        const config = this.config.content as GrdmappsConfigModel;
+        const workflows = JSON.parse(config.workflows);
+        const nodeWorkflows = JSON.parse(config.node_workflows);
+
+        let workflowId = '';
+        let url = '' ;
+
+        for(let i = 0; i < workflows.length; i++){
+            if(workflows[i].fields.workflow_description === workflow_desp){
+                workflowId = workflows[i].pk
+                for(let j = 0; j < nodeWorkflows.length; j++){
+                    if(nodeWorkflows[j].fields.workflow === workflowId){
+                        if(!nodeWorkflows[j].fields.scenarios){
+                            url = nodeWorkflows[j].fields.alternative_webhook_url
+                        }
+                    }
+                }
+            }
+        }
+
+        this.set('webhookUrl', url);
+
+        if(workflowType === 'web_meeting'){
+
+            this.set('showWorkflows', false);
+            this.set('showWebMeetingWorkflow', true);
+        }
+    }
+
+    @computed('config.all_web_meetings')
+    get all_web_meetings() {
+        if (!this.config) {
+            return '';
+        }
+        const config = this.config.content as GrdmappsConfigModel;
+        const all_web_meetings = JSON.parse(config.all_web_meetings);
+        return all_web_meetings;
+    }
+
+    @computed('config.upcoming_web_meetings')
+    get upcoming_web_meetings() {
+        if (!this.config) {
+            return '';
+        }
+        const config = this.config.content as GrdmappsConfigModel;
+        let upcoming_web_meetings = JSON.parse(config.upcoming_web_meetings);
+        let web_meeting_apps = JSON.parse(config.web_meeting_apps);
+
+        let previousDatetime;
+        let currentDatetime;
+        let previousDate = '';
+        let currentDate = '';
+
+        for(let i = 0; i < upcoming_web_meetings.length; i++){
+
+            //for display App Name on meeting list
+            for(let j = 0; j < web_meeting_apps.length; j++){
+                if(upcoming_web_meetings[i].fields.app === web_meeting_apps[j].pk){
+                    upcoming_web_meetings[i]['app_name_disp'] = this.camel2space(web_meeting_apps[j].fields.app_name);
+                    break;
+                }
+            }
+
+            //for display Date Bar
+            if(i === 0){
+                upcoming_web_meetings[i]['date_bar'] = false;
+            }else if(i !== 0){
+
+                previousDatetime =new Date(upcoming_web_meetings[i-1].fields.start_datetime);
+                currentDatetime =new Date(upcoming_web_meetings[i].fields.start_datetime);
+
+                previousDate = previousDatetime.getFullYear() + '/' + (previousDatetime.getMonth() + 1) + '/' + previousDatetime.getDate();
+                currentDate = currentDatetime.getFullYear() + '/' + (currentDatetime.getMonth() + 1) + '/' + currentDatetime.getDate();
+
+                if(currentDate != previousDate){
+                    upcoming_web_meetings[i]['date_bar'] = true;
+                }else{
+                    upcoming_web_meetings[i]['date_bar'] = false;
+                }
+            }
+        }
+        return upcoming_web_meetings;
+    }
+
+    @computed('config.previous_web_meetings')
+    get previous_web_meetings() {
+        if (!this.config) {
+            return '';
+        }
+        const config = this.config.content as GrdmappsConfigModel;
+        let previous_web_meetings = JSON.parse(config.previous_web_meetings);
+        let web_meeting_apps = JSON.parse(config.web_meeting_apps);
+
+        let currentDatetime;
+        let nextDatetime;
+        let nextDate = '';
+        let currentDate = '';
+
+        for(let i = 0; i < previous_web_meetings.length; i++){
+
+            //for display App Name on meeting list
+            for(let j = 0; j < web_meeting_apps.length; j++){
+                if(previous_web_meetings[i].fields.app === web_meeting_apps[j].pk){
+                    previous_web_meetings[i]['app_name_disp'] = this.camel2space(web_meeting_apps[j].fields.app_name);
+                    break;
+                }
+            }
+
+            if(i === 0){
+                previous_web_meetings[i]['date_bar'] = false;
+            }else if(i !== 0){
+
+                nextDatetime = new Date(previous_web_meetings[i-1].fields.start_datetime);
+                currentDatetime = new Date(previous_web_meetings[i].fields.start_datetime);
+
+                nextDate = nextDatetime.getFullYear() + '/' + (nextDatetime.getMonth() + 1) + '/' + nextDatetime.getDate();
+                currentDate = currentDatetime.getFullYear() + '/' + (currentDatetime.getMonth() + 1) + '/' + currentDatetime.getDate();
+
+                if(currentDate != nextDate){
+                    previous_web_meetings[i]['date_bar'] = true;
+                }else{
+                    previous_web_meetings[i]['date_bar'] = false;
+                }
+            }
+        }
+
+        return previous_web_meetings;
+    }
+
+    @computed('config.node_microsoft_teams_attendees')
+    get node_microsoft_teams_attendees() {
+        if (!this.config) {
+            return '';
+        }
+        const config = this.config.content as GrdmappsConfigModel;
+        const node_microsoft_teams_attendees = JSON.parse(config.node_microsoft_teams_attendees);
+        return node_microsoft_teams_attendees;
+    }
+
+    @computed('config.node_webex_meetings_attendees')
+    get node_webex_meetings_attendees() {
+        if (!this.config) {
+            return '';
+        }
+        const config = this.config.content as GrdmappsConfigModel;
+        const node_webex_meetings_attendees = JSON.parse(config.node_webex_meetings_attendees);
+        return node_webex_meetings_attendees;
+    }
+
+    @computed('config.workflows')
+    get workflows() {
+        if (!this.config) {
+            return '';
+        }
+        const config = this.config.content as GrdmappsConfigModel;
+        const workflows = JSON.parse(config.workflows);
+        return workflows;
+    }
+
+    @computed('config.web_meeting_apps')
+    get web_meeting_apps() {
+        if (!this.config) {
+            return '';
+        }
+        const config = this.config.content as GrdmappsConfigModel;
+        const web_meeting_apps = JSON.parse(config.web_meeting_apps);
+
+        for(let i = 0; i< web_meeting_apps.length; i++){
+
+            web_meeting_apps[i]['app_name_disp'] = this.camel2space(web_meeting_apps[i].fields.app_name)
+        }
+
+        return web_meeting_apps;
     }
 
     @computed('node')

07a3d75d270b4c40017f29c74dc22f053d2caddf
west1636 <nishikawa@nii.ac.jp>
2021-10-11 21:16:33 +0900

    change json structure of webexMeetingDeleteInviteesIds(1-2)

--- app/guid-node/grdmapps/controller.ts
+++ app/guid-node/grdmapps/controller.ts
@@ -73,7 +73,7 @@ interface webexMeetingsCreateInvitee {
 }
 
 interface webexMeetingsDeleteInvitee {
-    meetingId: string;
+    inviteeId: string;
 }
 
 interface payload {
@@ -928,7 +928,7 @@ export default class GuidNodeGrdmapps extends Controller {
                     if(this.webMeetingPk === nodeWebMeetingAttendeesRelation[j].fields.all_meeting_information){
                         if(arrayDeleteAttendeePks[i] === nodeWebMeetingAttendeesRelation[j].fields.attendees){
 
-                            webexMeetingsDeleteInviteeIds.push({'meetingId': nodeWebMeetingAttendeesRelation[j].fields.webex_meetings_invitee_id});
+                            webexMeetingsDeleteInviteeIds.push({'inviteeId': nodeWebMeetingAttendeesRelation[j].fields.webex_meetings_invitee_id});
                         }
                     }
                 }

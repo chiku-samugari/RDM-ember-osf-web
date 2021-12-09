e1ffc81355bc5ffb14c2d5ac03c7f0567764a91c
west1636 <nishikawa@nii.ac.jp>
2021-12-09 17:06:06 +0900

    Revert "resolve travis error formattedTimeSince"
    This reverts commit 8810369f155ebd2ebb1916da3874d65d6ffe6726.

--- app/utils/formatted-time-since.ts
+++ app/utils/formatted-time-since.ts
@@ -5,8 +5,8 @@ import moment from 'moment';
  * @param dateTime: reference time used to calculate relative time. Should be in the past.
  */
 export default function formattedTimeSince(dateTime: Date) {
-    const now = moment();
-    let then = moment(dateTime);
+    const now = moment.utc();
+    let then = moment.utc(dateTime);
     then = then > now ? now : then;
     return then.fromNow();
 }

7817fb9c083a49f5cb455ded8715aaf5866db462
west1636 <nishikawa@nii.ac.jp>
2022-09-05 11:16:07 +0900

    add webmeetings for web meetings addons

---
+++ app/models/webmeetings-config.ts
@@ -0,0 +1,24 @@
+import DS from 'ember-data';
+import OsfModel from './osf-model';
+
+const { attr } = DS;
+
+export default class WebMeetingsConfigModel extends OsfModel {
+    @attr('string') webMeetingsApps!: string;
+    @attr('string') allUpcomingWebMeetings!: string;
+    @attr('string') allPreviousWebMeetings!: string;
+    @attr('string') appNameMicrosoftTeams!: string;
+    @attr('string') appNameWebexMeetings!: string;
+    @attr('string') appNameZoomMeetings!: string;
+    @attr('string') nodeMicrosoftTeamsAttendees!: string;
+    @attr('string') nodeWebexMeetingsAttendees!: string;
+    @attr('string') nodeWebexMeetingsAttendeesRelation!: string;
+    @attr('string') institutionUsers!: string;
+    @attr('string') microsoftTeamsSignature!: string;
+}
+
+declare module 'ember-data/types/registries/model' {
+    export default interface ModelRegistry {
+        'webmeetings-config': WebMeetingsConfigModel;
+    } // eslint-disable-line semi
+}

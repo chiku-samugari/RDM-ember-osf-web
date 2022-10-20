4a2c017a671108a9cea4691d91a689e3258c99b0
west1636 <nishikawa@nii.ac.jp>
2022-10-20 12:53:35 +0900

    resolve travis error

--- lib/osf-components/addon/components/selected-item-attendees/component.ts
+++ lib/osf-components/addon/components/selected-item-attendees/component.ts
@@ -1,10 +1,10 @@
-import Component from '@ember/component';
 import { tagName } from '@ember-decorators/component';
+import Component from '@ember/component';
 import { layout } from 'ember-osf-web/decorators/component';
 import styles from './styles';
 import template from './template';
 
 @tagName('')
 @layout(template, styles)
-export default class selectedItemAttendees extends Component {
+export default class SelectedItemAttendees extends Component {
 }

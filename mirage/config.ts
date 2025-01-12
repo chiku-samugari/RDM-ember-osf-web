d09d4da1b185bceed2b4523a423808c2344cc382
chiku <samugari.penguin@gmail.com>
2025-01-12 21:37:32 +0900

    Add custom base image registration and inheritance feature.
    findImageByUrl must refer custom base images in addition.
    
    Use the term `image reference` instead of `image url` in case of custom
    base images.
    
    Custom Base Images must be identified by its image reference and level.
    
    Add custom base image delete confirmation.
    
    Show image url or image reference of base images on the project editor.
    
    Avoid to submit custom base image if name and/or image reference are
    emtpy.
    
    Pick recommended base image if the deleted image was the selected one.
    
    Custom base images must be identified by image references.
    
    Introduce new ImageCategory, USER.
    
    Rename custom image to singlespeed image.
    
    Utilize ancestor and linked project's custom base images. The API
    returns only the projects that the use have the READ persmission.
    
    Inform the user of the custom base image registration success.
    
    Give the user a chance to edit the inheriting custom base image.
    
    Rename BasicImageEntry component to BaseImageEntry.
    
    Use `onClick` property instead of `on` modifier if any.
    
    This seems one of the reasons for HBS linter error.
    
    Add buttons on CustomBaseImageEntry that offers delete and edit feature.
    
    Show dialogue if an custom base image is deleted while its editing.
    
    Rename performResetDirtyFragileFiles to performResetFile.
    
    Delete config files other than Dockerfile on custom base image picking.
    
    Reject image reference starts with `#repo2docker`.
    
    Return singlespeed image if it fails to find out the selected image.
    
    Add some test cases for custom base image.

--- mirage/config.ts
+++ mirage/config.ts
@@ -6,6 +6,7 @@ import { getCitation } from './views/citation';
 import { searchCollections } from './views/collection-search';
 import { reportDelete } from './views/comment';
 import { createBibliographicContributor } from './views/contributor';
+import * as customBaseImage from './views/custom-base-image';
 import { createDeveloperApp, updateDeveloperApp } from './views/developer-app';
 import { createDraftRegistration } from './views/draft-registration';
 import {
@@ -263,6 +264,7 @@ export default function(this: Server) {
     this.post('/project/:pid/binderhub/server_annotation', serverAnnotation.create);
     this.del('/project/:pid/binderhub/server_annotation/:aid', serverAnnotation.del);
     this.get('/project/:pid/binderhub/matlab/product_name_list/:release', matlabProductNameList.read);
+    this.get('/project/:pid/binderhub/custom_base_image', customBaseImage.read);
     this.get('/project/:id/metadata/erad/candidates', metadataNodeErad);
     this.get('/project/:id/metadata/project', metadataNodeProject);
 

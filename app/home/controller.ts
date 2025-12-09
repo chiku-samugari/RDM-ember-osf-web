0f91dc226ca77368ae6c83960b616fbb2efc361e
hide_24 <hidetoshi.yoshimoto@gmail.com>
2025-12-09 14:25:54 +0900

    Revert "Add login access control function based on authentication attributes and email address"

--- app/home/controller.ts
+++ app/home/controller.ts
@@ -18,16 +18,9 @@ const {
 export default class Home extends Controller {
     @service features!: Features;
 
-    queryParams = [{
-        loginNotAvailable: {
-            as: 'login_not_available',
-        },
-    }];
-
     organization: string = organization;
     brand: string = longBrand;
     useSimplePage: boolean = simplePage;
-    loginNotAvailable: boolean = false;
 
     get heroStyle(): string {
         return simplePage ? 'height: 600px !important;' : 'min-height: auto;';
